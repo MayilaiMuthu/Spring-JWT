d50bb691d0a80326a6f32cc78f1dcc9d7d6e0d9a
MayilaiMuthu <muthube2020@gmail.com>
2023-08-08 04:11:48 +0530

    Initial commit........

---
+++ spring-jwt-service/src/main/java/com/example/demo/config/ApplicationConstants.java
@@ -0,0 +1,14 @@
+package com.example.demo.config;
+
+/**
+ * @author MayilaiMuthu
+ * @since 08-Aug-2023
+ *
+ */
+public class ApplicationConstants {
+	
+	public final static String TEST_URL = "/api/customer-info/health-check";
+	
+	public final static String REGISTER_URL = "/api/customer-info/register";
+
+}
